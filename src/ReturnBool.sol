c021ae71595b301387a4849cbb634f569123a27d
tanim0la <izukfred@gmail.com>
2025-01-14 11:37:28 +0100

    improve comment

--- src/ReturnBool.sol
+++ src/ReturnBool.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.13;
 contract ReturnBool {
     function main() external pure returns (bool) {
         assembly {
-            // your code here
-            // return true
+            // this code already works. Use it to test your setup.
+            // the goal here is to return true
 
+            // solution
             mstore(0x00, 0x01)
-
             return(0x00, 0x20)
         }
     }
