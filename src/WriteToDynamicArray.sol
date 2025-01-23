4b0f410ed69bce125ae540f09c1c8255adc669a8
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 18:50:54 +0100

    24. WriteToDynamicArray done

--- src/WriteToDynamicArray.sol
+++ src/WriteToDynamicArray.sol
@@ -9,6 +9,16 @@ contract WriteToDynamicArray {
             // your code here
             // store the values in the DYNAMIC array `x` in the storage variable `writeHere`
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            let offset := calldataload(0x04)
+            let length := calldataload(add(0x04, offset))
+            sstore(0, length)
+            mstore(0x00, 0x00)
+            let h := keccak256(0x00, 0x20)
+            let offsetValue := add(0x24, offset)
+            for { let i := 0 } lt(i, length) { i := add(i, 1) } {
+                sstore(add(h, i), calldataload(offsetValue))
+                offsetValue := add(0x20, offsetValue)
+            }
         }
     }
 
