ae302e154c07a4d78efcc27cf15a455aa6ea252a
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 15:15:04 +0100

    4. RevertWithError done

--- src/RevertWithError.sol
+++ src/RevertWithError.sol
@@ -7,6 +7,12 @@ contract RevertWithError {
             // revert the function with an error of type `Error(string)`
             // use "RevertRevert" as error message
             // Hint: The error type is a predefined four bytes. See https://www.rareskills.io/post/try-catch-solidity
+            let selector := shl(224, 0x08c379a0)
+            mstore(0x00, selector)
+            mstore(0x04, 0x20)
+            mstore(0x24, 12)
+            mstore(0x44, "RevertRevert")
+            revert(0x00, 0x64)
         }
     }
 }
