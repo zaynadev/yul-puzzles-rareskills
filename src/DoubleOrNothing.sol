6c8c2feb944a75ca78918df1140b7b29c17fbc04
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 15:40:13 +0100

    9. DoubleOrNothing

--- src/DoubleOrNothing.sol
+++ src/DoubleOrNothing.sol
@@ -9,7 +9,10 @@ contract DoubleOrNothing {
             // 21 or less. If 2 * x > 21, then
             // return 0.
             // Hint: check if x ≤ 10, which is equivalent to x < 11
-
+            let product := 0
+            if lt(mul(x, 2), 22) { product := mul(x, 2) }
+            mstore(0x00, product)
+            return(0x00, 0x20)
         }
     }
 }
