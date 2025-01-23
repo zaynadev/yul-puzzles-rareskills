d433ca7650723b7950f689aa2c8c7d305810ca06
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 20:08:03 +0100

    32. WriteDynamicArrayToStorage

--- src/WriteDynamicArrayToStorage.sol
+++ src/WriteDynamicArrayToStorage.sol
@@ -8,6 +8,16 @@ contract WriteDynamicArrayToStorage {
         assembly {
             // your code here
             // write the dynamic calldata array `x` to storage variable `writeHere`
+            let offset := calldataload(0x04)
+            let length := calldataload(add(0x04, offset))
+            let offsetValue := add(0x24, offset)
+            sstore(0, length)
+            mstore(0x00, 0x00)
+            let h := keccak256(0x00, 0x20)
+            for { let i := 0 } lt(i, length) { i := add(i, 1) } {
+                sstore(add(h, i), calldataload(offsetValue))
+                offsetValue := add(offsetValue, 0x20)
+            }
         }
     }
 }
