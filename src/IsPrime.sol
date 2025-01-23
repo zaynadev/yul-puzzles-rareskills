764e2316b870f4cbd2b8f6e886393d2eaac11906
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 16:35:40 +0100

    11. IsPrime done

--- src/IsPrime.sol
+++ src/IsPrime.sol
@@ -9,6 +9,20 @@ contract IsPrime {
             // 1. check if the number is a multiple of 2 or 3
             // 2. loop from 5 to x / 2 to see if it is divisible
             // 3. increment the loop by 2 to skip the even numbers
+            let isPrime := 0x01
+            switch x
+            case 1 { isPrime := 0x00 }
+            default {
+                for { let i := 2 } lt(i, div(x, 2)) { i := add(i, 1) } {
+                    if iszero(mod(x, i)) {
+                        isPrime := 0x00
+                        break
+                    }
+                }
+            }
+
+            mstore(0x00, isPrime)
+            return(0x00, 0x20)
         }
     }
 }
