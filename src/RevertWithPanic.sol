dd587af45cb702d3d481409a852f5b4921430301
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 15:22:05 +0100

    6. RevertWithPanic done

--- src/RevertWithPanic.sol
+++ src/RevertWithPanic.sol
@@ -8,6 +8,10 @@ contract RevertWithPanic {
             // revert the function with an error of type `Panic(uint256)`
             // use "0x01" as error code
             // Hint: The error type is built-in and cannot be re-defined. See https://www.rareskills.io/post/try-catch-solidity
+            let selector := shl(224, 0x4e487b71)
+            mstore(0x00, selector)
+            mstore(0x04, 0x01)
+            revert(0x00, 0x24)
         }
     }
 }
