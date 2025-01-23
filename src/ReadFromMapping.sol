01209e56cedf2b78f41fecadd15451bef92fe330
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 19:15:12 +0100

    25. ReadFromMapping done

--- src/ReadFromMapping.sol
+++ src/ReadFromMapping.sol
@@ -14,6 +14,11 @@ contract ReadFromMapping {
             // read the value at the `index` in the mapping `readMe`
             // and return it
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            mstore(0x00, index)
+            mstore(0x20, 0x00)
+            let h := keccak256(0x00, 0x40)
+            mstore(0x00, sload(h))
+            return(0x00, 0x20)
         }
     }
 }
