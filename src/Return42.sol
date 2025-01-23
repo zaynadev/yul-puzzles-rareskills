459145a73c0f4874f995766f4b7ee369039b7abc
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 15:06:22 +0100

    3. Return42 done

--- src/Return42.sol
+++ src/Return42.sol
@@ -2,17 +2,19 @@
 pragma solidity ^0.8.13;
 
 contract Return42 {
-
-  function main() external pure returns (uint256) {
-      assembly {
-          // your code here
-          // return 42 in decimal
-          // hint: return works the same way as revert
-          // simply replace `revert(start, len)` with
-          // `return(start, len)`
-          // hint: you need to return 32 bytes of size
-          // even though the returned value is 42
-          // https://www.rareskills.io/post/assembly-revert
-      }
-  }
+    function main() external pure returns (uint256) {
+        assembly {
+            // your code here
+            // return 42 in decimal
+            // hint: return works the same way as revert
+            // simply replace `revert(start, len)` with
+            // `return(start, len)`
+            // hint: you need to return 32 bytes of size
+            // even though the returned value is 42
+            // https://www.rareskills.io/post/assembly-revert
+            let data := 42
+            mstore(0x00, data)
+            return(0x00, 0x20)
+        }
+    }
 }
