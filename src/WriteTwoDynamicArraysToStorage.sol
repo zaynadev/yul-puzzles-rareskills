df9b797ec619a04ae16fe9dfe80b8d96e32a6637
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 20:51:44 +0100

    33. WriteTwoDynamicArraysToStorage done

--- src/WriteTwoDynamicArraysToStorage.sol
+++ src/WriteTwoDynamicArraysToStorage.sol
@@ -10,6 +10,26 @@ contract WriteTwoDynamicArraysToStorage {
             // your code here
             // write the dynamic calldata array `x` to storage variable `writeHere1` and
             // dynamic calldata array `y` to storage variable `writeHere2`
+            let offset1 := calldataload(0x04)
+            let offset2 := calldataload(0x24)
+            let length1 := calldataload(add(0x04, offset1))
+            let length2 := calldataload(add(0x04, offset2))
+            let offsetValue1 := add(0x24, offset1)
+            let offsetValue2 := add(0x24, offset2)
+            mstore(0x00, 0x00)
+            let h1 := keccak256(0x00, 0x20)
+            mstore(0x00, 0x01)
+            let h2 := keccak256(0x00, 0x20)
+            sstore(0, length1)
+            sstore(1, length2)
+            for { let i := 0 } lt(i, length1) { i := add(i, 1) } {
+                sstore(add(h1, i), calldataload(offsetValue1))
+                offsetValue1 := add(offsetValue1, 0x20)
+            }
+            for { let i := 0 } lt(i, length2) { i := add(i, 1) } {
+                sstore(add(h2, i), calldataload(offsetValue2))
+                offsetValue2 := add(offsetValue2, 0x20)
+            }
         }
     }
 }
