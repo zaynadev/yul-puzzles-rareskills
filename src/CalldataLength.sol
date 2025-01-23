dca18b9a401be63347cc96e762d76cd665b38220
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 16:50:05 +0100

    12. CalldataLength done

--- src/CalldataLength.sol
+++ src/CalldataLength.sol
@@ -7,6 +7,8 @@ contract CalldataLength {
             // your code here
             // return the length of the calldata
             // hint: calldatasize opcode
+            mstore(0x00, calldatasize())
+            return(0x00, 0x20)
         }
     }
 }
