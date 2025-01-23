91cb2842895eb7abeb0636459a891cc5852f689f
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 20:01:58 +0100

    31. WriteToDoubleMapping done

--- src/WriteToDoubleMapping.sol
+++ src/WriteToDoubleMapping.sol
@@ -9,6 +9,13 @@ contract WriteToDoubleMapping {
             // your code here
             // set the `value` for a `user` and a `token`
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            mstore(0x00, user)
+            mstore(0x20, 0x00)
+            let h1 := keccak256(0x00, 0x40)
+            mstore(0x00, token)
+            mstore(0x20, h1)
+            let h2 := keccak256(0x00, 0x40)
+            sstore(h2, value)
         }
     }
 }
