b85558fe6b660588efb7861a847d2d3c4a45689f
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 17:32:36 +0100

    17. MaxOfArray done

--- src/MaxOfArray.sol
+++ src/MaxOfArray.sol
@@ -7,6 +7,21 @@ contract MaxOfArray {
             // your code here
             // return the maximum value in the array
             // revert if array is empty
+            let offset := calldataload(0x04)
+            let length := calldataload(add(0x04, offset))
+            switch length
+            case 0 { revert(0, 0) }
+            default {
+                let offsetValue := add(0x24, offset)
+                let max := calldataload(offsetValue)
+                for { let i := 1 } lt(i, length) { i := add(i, 1) } {
+                    offsetValue := add(0x20, offsetValue)
+                    let value := calldataload(offsetValue)
+                    if gt(value, max) { max := value }
+                }
+                mstore(0x00, max)
+                return(0x00, 0x20)
+            }
         }
     }
 }
