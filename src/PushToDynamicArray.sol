606d3427fcf70b2d0292bd0449f517b25edff365
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 19:26:09 +0100

    27. PushToDynamicArray done

--- src/PushToDynamicArray.sol
+++ src/PushToDynamicArray.sol
@@ -9,6 +9,11 @@ contract PushToDynamicArray {
             // your code here
             // push the newValue to the dynamic array `pushToMe`
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            mstore(0x00, 0x00)
+            let h := keccak256(0x00, 0x20)
+            let length := sload(0)
+            sstore(add(length, h), newValue)
+            sstore(0, add(length, 1))
         }
     }
 
