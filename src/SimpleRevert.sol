7867a29da45a62492bb160019f5ecdf780bba244
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 14:29:53 +0100

    1. SimpleRevert done

--- src/SimpleRevert.sol
+++ src/SimpleRevert.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.13;
 
 contract SimpleRevert {
-
-  function main() external pure {
-      assembly {
-          // your code here
-          // revert with no message
-          // hint: https://www.rareskills.io/post/assembly-revert
-      }
-  }
+    function main() external pure {
+        assembly {
+            // your code here
+            // revert with no message
+            // hint: https://www.rareskills.io/post/assembly-revert
+            revert(0, 0)
+        }
+    }
 }
