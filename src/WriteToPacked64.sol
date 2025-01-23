c76561755f426748314136bb320b070b22a7a68c
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-23 23:23:21 +0100

    37. WriteToPacked64 done

--- src/WriteToPacked64.sol
+++ src/WriteToPacked64.sol
@@ -12,6 +12,12 @@ contract WriteToPacked64 {
             // your code here
             // change the value of `writeHere` storage variable to `v`
             // be careful not to alter the value of `someValue` variable
+            let value := sload(0)
+            let mask1 := sub(shl(64, 1), 1)
+            let mask2 := shl(128, sub(shl(128, 1), 1))
+            let mask := or(mask1, mask2)
+            value := and(value, mask)
+            sstore(0, or(value, shl(64, v)))
         }
     }
 }
