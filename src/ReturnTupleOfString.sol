94251a35aadb6af1a7cc9dde4b63428306fe069a
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-24 21:50:26 +0100

    57. ReturnTupleOfString done

--- src/ReturnTupleOfString.sol
+++ src/ReturnTupleOfString.sol
@@ -6,6 +6,14 @@ contract ReturnTupleOfString {
         assembly {
             // your code here
             // return the tuple of string: ("Hello", "RareSkills")
+
+            mstore(0x00, 0x40)
+            mstore(0x20, 0x80)
+            mstore(0x40, 5)
+            mstore(0x60, "Hello")
+            mstore(0x80, 10)
+            mstore(0xa0, "RareSkills")
+            return(0x00, 0xc0)
         }
     }
 }
