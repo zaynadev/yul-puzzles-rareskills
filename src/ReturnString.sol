27b34b61ebe877896d6896698616c945458387f8
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-24 21:06:33 +0100

    54. ReturnString done

--- src/ReturnString.sol
+++ src/ReturnString.sol
@@ -6,6 +6,10 @@ contract ReturnString {
         assembly {
             // your code here
             // return the exact string: `Hello, RareSkills`
+            mstore(0x00, 0x20)
+            mstore(0x20, 17)
+            mstore(0x40, "Hello, RareSkills")
+            return(0x00, 0x60)
         }
     }
 }
