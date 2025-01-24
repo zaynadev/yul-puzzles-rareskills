4d8dd6fdd4d222f5bffce2ae80f7115181d53e34
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-01-24 14:38:19 +0100

    40. EventWithoutData done

--- src/EventWithoutData.sol
+++ src/EventWithoutData.sol
@@ -12,6 +12,9 @@ contract EventWithoutData {
             // use `log1` to emit the event with one topic, which is the event's signature hash
             // Hint: Calculate the event signature hash using `keccak256("MyEvent()")`
             // The event hash serves as `topic0` in the log
+            mstore(0x00, "MyEvent()")
+            let sig := keccak256(0x00, 0x09)
+            log1(0x00, 0x00, sig)
         }
     }
 }
