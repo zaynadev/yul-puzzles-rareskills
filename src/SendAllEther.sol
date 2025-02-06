31790165a749f6c824962c172ad454e25ea0da19
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-02-06 20:31:08 +0100

    62. SendAllEther done

--- src/SendAllEther.sol
+++ src/SendAllEther.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.13;
 
 contract SendAllEther {
-
     function main(address payable to) external payable {
         assembly {
             // your code here
             // send the entire contract balance to `to`
             // hint: use the `call` opcode: https://docs.soliditylang.org/en/latest/yul.html#evm-dialect
             // hint: use the `selfbalance()` opcode
-
+            let ok := call(gas(), to, selfbalance(), 0, 0, 0, 0)
         }
     }
 }
