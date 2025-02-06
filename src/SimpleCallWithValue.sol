ae133f29da7858e25e9074dd556c0260e62f7534
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-02-06 22:23:49 +0100

    67. SimpleCallWithValue done

--- src/SimpleCallWithValue.sol
+++ src/SimpleCallWithValue.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.13;
 
 contract SimpleCallWithValue {
-
     function main(address t) external payable {
         assembly {
             // your code here
             // call "t.foo()" while sending msg.value
             // hint: "foo()" has function selector 0xc2985578
             // hint: callvalue() returns the value of the current call
-       }
+            mstore(0x00, 0xc2985578)
+            let ok := call(gas(), t, callvalue(), 0x1c, 0x04, 0, 0)
+        }
     }
 }
