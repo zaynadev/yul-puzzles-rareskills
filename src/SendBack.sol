36973e07bafa0d309ccb7ab9d4a984f942eadd31
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-02-06 21:18:55 +0100

    64. SendBack done

--- src/SendBack.sol
+++ src/SendBack.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.13;
 
 contract SendBack {
-
     fallback() external payable {
         assembly {
             // your code here
             // whatever amount of ether is sent to the contract, send it back to the sender
             // hint: use callvalue() to get the amount of ether sent to the contract
+            let ok := call(gas(), caller(), callvalue(), 0, 0, 0, 0)
         }
     }
 }
