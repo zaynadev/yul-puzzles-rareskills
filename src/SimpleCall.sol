8a2d4c8ca9fe2849a76df1da6183a85e4b60926e
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-02-06 22:21:16 +0100

    66. SimpleCall done

--- src/SimpleCall.sol
+++ src/SimpleCall.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.13;
 
 contract SimpleCall {
-
     function main(address t) external payable {
         assembly {
             // your code here
             // call "t.foo()"
             // hint: "foo()" has function selector 0xc2985578
-       }
+            mstore(0x00, 0xc2985578)
+            let ok := call(gas(), t, callvalue(), 0x1c, 0x04, 0, 0)
+        }
     }
 }
