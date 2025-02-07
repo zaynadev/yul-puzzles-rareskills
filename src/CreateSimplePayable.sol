b6eeac6cece876a1bb84ae806cd4eca4a8cba4b2
RareSkills <114334277+RareSkills@users.noreply.github.com>
2025-02-07 03:01:54 -0800

    Update CreateSimplePayable.sol

--- src/CreateSimplePayable.sol
+++ src/CreateSimplePayable.sol
@@ -11,9 +11,6 @@ contract CreateSimplePayable {
             // do this as part of create, don't transfer it after
             // hint: use the `create` opcode
             // hint: the bytecode is already in memory
-            let adr := create(callvalue(), add(deploymentBytecode, 32), deploymentBytecode)
-            mstore(0, adr)
-            return(0, 32)
         }
     }
 }
