dbdf74647c7c862912e8b1b48f0c03e41ecfdfbe
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-02-13 20:54:49 +0100

    78. AbsoluteValue done

--- src/AbsoluteValue.sol
+++ src/AbsoluteValue.sol
@@ -8,6 +8,18 @@ contract AbsoluteValue {
             // return the absolute value of x
             // hint: use signed comparisons
             // hint: https://www.rareskills.io/post/signed-int-solidity
+            // slt(x, 0) returns 1 if x < 0, and 0 otherwise
+            switch slt(x, 0)
+            case 0 {
+                // If x >= 0, just return x as is
+                mstore(0, x)
+                return(0, 0x20)
+            }
+            case 1 {
+                // If x < 0, return -x
+                mstore(0, sub(0, x))
+                return(0, 0x20)
+            }
         }
     }
 }
